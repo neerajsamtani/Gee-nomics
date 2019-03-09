70ecbc60c63517a59d5a55862b371c3b865e2489
Neeraj Samtani <neerajsamtani@g.ucla.edu>
2019-03-08 16:35:48 -0800

    Genome class complete

--- Gee-nomics/Genome.cpp
+++ Gee-nomics/Genome.cpp
@@ -14,31 +14,115 @@ public:
     string name() const;
     bool extract(int position, int length, string& fragment) const;
 private:
+	string m_nm;
+	string m_sequence;
 };
 
 GenomeImpl::GenomeImpl(const string& nm, const string& sequence)
-{
-    // This compiles, but may not be correct
-}
+	:m_nm(nm), m_sequence(sequence)
+{}
 
 bool GenomeImpl::load(istream& genomeSource, vector<Genome>& genomes) 
 {
-    return false;  // This compiles, but may not be correct
+	/*
+	Improper formatting includes things like not starting with a name line,
+	non-name lines containing any characters other than upper or lower case A C T G N, no
+	base lines after a name line, empty lines, or a line starting with a greater-than character
+	but containing no other characters.
+	*/
+	string currentLine;
+	string name = "";
+	string sequence = "";
+	while (getline(genomeSource, currentLine))
+	{
+		// Check for empty line
+		if (currentLine == "" || currentLine[0] == ' ')
+			return false;
+		// If it is a name line
+		if (currentLine[0] == '>')
+		{
+			// If there was a previous genome
+			// add it to the genome vector
+			if (name != "" && sequence != "")
+			{
+				genomes.push_back(Genome(name, sequence));
+				// reset name and sequence
+				name = "";
+				sequence = "";
+			}
+			// Error if there were no base lines after name line
+			// by checking if name has a value but sequence is empty
+			// Error if there was a line starting with > but no other characters
+			// by checking if name is empty but sequence has a value
+			// TODO: is this necessary : ( ... || name == "" && sequence != "")
+			else if (name != "" && sequence == "")
+			{
+				return false;
+			}
+			name = currentLine.substr(1);
+			// Check if there was a line starting with >
+			// character but containing no other characters.
+			if (name.length() == 0)
+				return false;
+		}
+		// It is a sequence
+		else
+		{
+			// Check if it didn't start with a name line
+			if (name == "")
+				return false;
+			// Check if all characters in the sequence are valid
+			for (int i = 0; i < currentLine.length(); i++)
+			{
+				char currentChar = toupper(currentLine[i]);
+				if (currentChar == 'A' ||
+					currentChar == 'C' ||
+					currentChar == 'T' ||
+					currentChar == 'G' ||
+					currentChar == 'N')
+					sequence += currentChar;
+				else
+					return false;
+			}
+		}
+	}
+	// end of file was reached
+	// try to add the last genome
+	if (name != "" && sequence != "")
+	{
+		genomes.push_back(Genome(name, sequence));
+	}
+	// Error if there were no base lines after name line
+	// by checking if name has a value but sequence is empty
+	// Error if there was a line starting with > but no other characters
+	// by checking if name is empty but sequence has a value
+	else if (name != "" && sequence == "" ||
+				name == "" && sequence != "")
+	{
+		return false;
+	}
+	return true;
 }
 
 int GenomeImpl::length() const
 {
-    return 0;  // This compiles, but may not be correct
+	return m_sequence.length();
 }
 
 string GenomeImpl::name() const
 {
-    return "";  // This compiles, but may not be correct
+	return m_nm;
 }
 
 bool GenomeImpl::extract(int position, int length, string& fragment) const
 {
-    return false;  // This compiles, but may not be correct
+	if ((position + length) > this->length())
+		return false;
+	else
+	{
+		fragment = m_sequence.substr(position, length);
+		return true;
+	}
 }
 
 //******************** Genome functions ************************************
---
+++ Gee-nomics/main.cpp
@@ -0,0 +1,76 @@
+#include "provided.h"
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
+#include <vector>
+using namespace std;
+
+void testGenome()
+{
+	// Constructor
+	Genome GenomeOne("Genome 1", "ACTG");
+	Genome GenomeTwo("Genome 2", "TCGACT");
+	Genome GenomeThree("Genome 3", "TCTCG");
+	Genome GenomeEmpty("", "");
+
+	// name() and length()
+	assert(GenomeOne.name() == "Genome 1" && GenomeOne.length() == 4);
+	assert(GenomeTwo.name() == "Genome 2" && GenomeTwo.length() == 6);
+	assert(GenomeThree.name() == "Genome 3" && GenomeThree.length() == 5);
+	assert(GenomeEmpty.name() == "" && GenomeEmpty.length() == 0);
+
+	// extract()
+	string fragment;
+	// extract from start
+	assert(GenomeOne.extract(0, 0, fragment) && fragment.length() == 0);
+	assert(GenomeOne.extract(0, 1, fragment) && fragment.length() == 1);
+	assert(GenomeOne.extract(0, 2, fragment) && fragment.length() == 2);
+	assert(GenomeOne.extract(0, 3, fragment) && fragment.length() == 3);
+	assert(GenomeOne.extract(0, 4, fragment) && fragment.length() == 4);
+	assert(!GenomeOne.extract(0, 5, fragment) && fragment.length() == 4);
+	// extract from middle
+	assert(GenomeOne.extract(1, 0, fragment) && fragment.length() == 0);
+	assert(GenomeOne.extract(1, 1, fragment) && fragment.length() == 1);
+	assert(GenomeOne.extract(1, 2, fragment) && fragment.length() == 2);
+	assert(GenomeOne.extract(1, 3, fragment) && fragment.length() == 3);
+	assert(!GenomeOne.extract(1, 4, fragment) && fragment.length() == 3);
+	// extract from middle
+	assert(GenomeOne.extract(2, 0, fragment) && fragment.length() == 0);
+	assert(GenomeOne.extract(2, 1, fragment) && fragment.length() == 1);
+	assert(GenomeOne.extract(2, 2, fragment) && fragment.length() == 2);
+	assert(!GenomeOne.extract(2, 3, fragment) && fragment.length() == 2);
+	// extract from end
+	assert(GenomeOne.extract(3, 0, fragment) && fragment.length() == 0);
+	assert(GenomeOne.extract(3, 1, fragment) && fragment.length() == 1);
+	assert(!GenomeOne.extract(3, 2, fragment) && fragment.length() == 1);
+	// extract after end
+	assert(!GenomeOne.extract(4, 1, fragment) && fragment.length() == 1);
+	// extract from empty Genome
+	assert(!GenomeEmpty.extract(0, 1, fragment) && fragment.length() == 1);
+
+	// load()
+	string filename = "c:/genomes/Test_Genomes.txt";
+	ifstream strm(filename);
+	if (!strm)
+	{
+		cout << "Cannot	open " << filename << endl;
+		return;
+	}
+	vector<Genome> vg;
+	bool success = Genome::load(strm, vg);
+	if (success)
+	{
+		cout << "Loaded	" << vg.size() << " genomes successfully:" << endl;
+		for (int k = 0; k != vg.size(); k++)
+			cout << vg[k].name() << endl;
+	}
+	else
+		cout << "Error loading genome data" << endl;
+}
+
+int main()
+{
+	testGenome();
+	cout << "Passed all tests" << endl;
+}
